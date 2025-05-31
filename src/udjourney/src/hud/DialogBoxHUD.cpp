e1b9fd04b25f83c386e9c337c13df435131f8396
Quentin Cartier <daoliangshumaishuji@gmail.com>
2025-05-31 23:41:37 +0200

    Separate line wrapping from update

--- src/udjourney/src/hud/DialogBoxHUD.cpp
+++ src/udjourney/src/hud/DialogBoxHUD.cpp
@@ -3,33 +3,32 @@
 
 #include <raylib/raylib.h>
 
+#include <functional>
 #include <sstream>
 #include <string>
 #include <utility>
-#include <functional>
 
-DialogBoxHUD::DialogBoxHUD(Rectangle iRect) : m_rect(iRect) {
-    // Initialize the dialog box with a default message
-    m_text = "Hello, this is a dialog box!";
-    m_is_focusable = true;  // Set focusable to true
-}
+namespace internal {
 
-void DialogBoxHUD::update(float deltaTime) {
-    const int font_size = 20;
-    const int padding = 10;
-    float max_width = m_rect.width - 2 * padding;
+struct TextOptions {
+    int font_size;
+    int max_width;
+    int padding;
+} text_options;
 
-    m_wrapped_lines.clear();
-    std::istringstream stream(m_text);
+void wrap_text(const std::string& iText, TextOptions iTextOptions,
+               std::vector<std::string>& oWrappedLines) {
+    oWrappedLines.clear();
+    std::istringstream stream(iText);
     std::string word;
     std::string line;
 
     while (stream >> word) {
         std::string test_line = line.empty() ? word : line + " " + word;
-        int line_width = MeasureText(test_line.c_str(), font_size);
+        int line_width = MeasureText(test_line.c_str(), iTextOptions.font_size);
 
-        if (line_width > static_cast<int>(max_width)) {
-            m_wrapped_lines.push_back(line);
+        if (line_width > static_cast<int>(iTextOptions.max_width)) {
+            oWrappedLines.push_back(line);
             line = word;
         } else {
             line = test_line;
@@ -37,8 +36,33 @@ void DialogBoxHUD::update(float deltaTime) {
     }
 
     if (!line.empty()) {
-        m_wrapped_lines.push_back(line);
+        oWrappedLines.push_back(line);
     }
+}
+}  // namespace internal
+
+DialogBoxHUD::DialogBoxHUD(Rectangle iRect) : m_rect(iRect) {
+    // Initialize the dialog box with a default message
+    m_text = "Hello, this is a dialog box!";
+    m_is_focusable = true;  // Set focusable to true
+    internal::text_options.font_size = 20;
+    internal::text_options.padding = 10;
+    internal::text_options.max_width =
+        static_cast<int>(m_rect.width - 2 * internal::text_options.padding);
+    internal::wrap_text(m_text, internal::text_options, m_wrapped_lines);
+}
+
+void DialogBoxHUD::set_text(const std::string& text) {
+    m_text = text;
+    internal::wrap_text(m_text, internal::text_options, m_wrapped_lines);
+}
+void DialogBoxHUD::set_text(std::string&& text) {
+    m_text = std::move(text);
+    internal::wrap_text(m_text, internal::text_options, m_wrapped_lines);
+}
+
+void DialogBoxHUD::update(float deltaTime) {
+    // To implement
 }
 
 void DialogBoxHUD::draw() const {
@@ -59,7 +83,10 @@ void DialogBoxHUD::draw() const {
     }
 }
 
-void DialogBoxHUD::set_on_finished_callback(
-    std::function<void()> callback) {
+void DialogBoxHUD::set_on_finished_callback(std::function<void()> callback) {
     m_on_finished_callback = std::move(callback);
+}
+
+void DialogBoxHUD::set_on_next_callback(std::function<void()> callback) {
+    m_on_next_callback = std::move(callback);
 }
