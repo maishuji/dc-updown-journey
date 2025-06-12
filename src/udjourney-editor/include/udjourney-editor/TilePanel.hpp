382fa809624a68ee45b3a886af86ecc767286be3
Quentin Cartier <daoliangshumaishuji@gmail.com>
2025-06-12 22:31:20 +0200

    Add TilePanel

---
+++ src/udjourney-editor/include/udjourney-editor/TilePanel.hpp
@@ -0,0 +1,15 @@
+// Copyright 2025 Quentin Cartier
+#include <imgui.h>
+
+#include <string>
+
+class TilePanel {
+ public:
+    void draw();
+    inline ImU32 get_current_color() const noexcept { return cur_color; }
+
+ private:
+    void set_button(const std::string& iId, ImU32 color);
+    ImU32 cur_color = IM_COL32(255, 255, 255,
+                               255);  // Default color for the current selection
+};
